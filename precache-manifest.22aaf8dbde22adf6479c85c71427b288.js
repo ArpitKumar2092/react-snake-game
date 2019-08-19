024b423f8fe3af1d9f948b885ceb00fef052cd2c
ArpitKumar2092 <arpitkumar2092@gmail.com>
2019-08-20 01:58:36 +0530

    upload

---
+++ precache-manifest.22aaf8dbde22adf6479c85c71427b288.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4cf925090c681e7429a3f8cc4d3d5e0",
+    "url": "/react-snake-game/index.html"
+  },
+  {
+    "revision": "698ad057fc540073b7b0",
+    "url": "/react-snake-game/static/css/main.11db1da5.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/react-snake-game/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "698ad057fc540073b7b0",
+    "url": "/react-snake-game/static/js/main.1b4cba5b.chunk.js"
+  },
+  {
+    "revision": "2bb4e33f074ce70d55a3",
+    "url": "/react-snake-game/static/js/runtime~main.87448e3a.js"
+  }
+]);
